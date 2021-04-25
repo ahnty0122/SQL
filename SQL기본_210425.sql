d9eddea444374380b18dc1f38770abb5db246003
ahnty0122 <ahnty0122@naver.com>
2021-04-25 20:57:31 +0900

    SQL 기본, 활용

---
+++ SQL기본_210425.sql
@@ -0,0 +1,263 @@
+﻿-- 6
+
+-- Instructor 테이블에서 유일한 dept_name 출력
+use University;
+select distinct dept_name from instructor;
+-- Instructor 테이블에서 이름, salary, 1.1% 인상한 salary 출력 (별칭: 이름, 인상전, 인상후)
+select name as 이름, salary as 인상전, salary * 1.1 as 인상후 from instructor;
+
+-- Instructor 테이블에서 name 이름, salary(연봉)을 이용한 월급을 2번째 자리까지 반올림한 결과(월급여) 출력
+select name 이름, salary 인상전, round(salary / 12, 2) 월급여 from instructor;
+
+-- 0 학과 0 학생의 취득학점은 0 의 형식으로 출력. Dept_name(학과), name(학생), tot_cred(취득학점, int 형임)
+select dept_name + '학과 ' + name + ' 학생의 취득학점은 ' + convert(varchar, tot_cred) from student;
+
+-- SQL Expert 대문자로, 소문자로, a의 아스키 문자, 65에 해당하는 문자, rdbms와 sql 문자열 연결
+-- sql expert에서 5번째자리부터 길이 3 출력, 문자열 길이 함수, 왼쪽 공백 제거 함수(    sql expert)
+select UPPER('SQL Expert');
+select LOWER('SQL Expert');
+select ASCII('A');
+SELECT CHAR(65);
+SELECT CONCAT('RDBMS', ' SQL');
+SELECT SUBSTRING('SQL EXPERT', 5, 3);
+SELECT LEN('SQL EXPERT');
+SELECT LTRIM('   SQL EXPERT');
+
+-- 절대값 함수, 부호 알려주는 함수 음수면 -1 0이면 0 양수면 1, 반올림 함수, 올림, 내림, 지수함수, 거듭제곱, 루트
+SELECT ABS(-10);
+SELECT SIGN(-1), SIGN(0), SIGN(1);
+SELECT ROUND(7.537, 2);
+SELECT CEILING(7.5);
+SELECT FLOOR(7.5);
+SELECT EXP(10);
+SELECT POWER(2, 4);
+SELECT SQRT(4);
+
+-- 현재 날짜+시간 출력 함수 2가지, 요일 출력, 분기 출력, 오늘 기준 100일 뒤 날짜 구하기, 날짜 차이 구하기
+-- 현재 날짜 및 시간에서 날짜만 출력
+SELECT SYSDATETIME();
+SELECT GETDATE();
+SELECT DATENAME(WEEKDAY, GETDATE());
+SELECT DATEPART(QUARTER, GETDATE());
+SELECT DATEADD(DAY, 100, GETDATE());
+SELECT DATEDIFF(dd, GETDATE(), '2021-04-26');
+select convert(char(20), getdate(), 108);
+
+-- 지정한 날짜 형식의 부분만 출력  emp 테이블의 ename, hiredate에서 입사년, 입사월, 입사일 출력
+use company;
+select ename, year(hiredate) 입사년, month(hiredate) 입사월, day(hiredate) 입사일
+from emp;
+
+select ename, datepart(year, hiredate) 입사년, datepart(month, hiredate) 입사월, datepart(day, hiredate) 입사일
+from emp;
+
+-- instructor 테이블에서 salary를 int 형식으로 출력
+use University;
+select convert(int, salary) as 월급
+from instructor;
+
+select cast(salary as int) as 월급
+from instructor;
+
+-- emp 테이블에서 sal이 2000 초과인 데이터는 sal로 , 아니면 2000으로 출력(개정급여로)
+use COMPANY;
+select case when sal > 2000 then sal else 2000 end as 개정급여
+from emp;
+
+-- emp 테이블에서 ename, sal을 개정전으로, sal이 3000초과면 sal을 1.05 인상, sal이 2000초과면 sal 1.10 인상, 아니면 sal 1.20 인상한 값을 개정후 로 출력
+select ename, sal 개정전, case when sal > 3000 then sal * 1.05 when sal > 2000 then sal * 1.10 else sal * 1.20 end 개정후
+from emp;
+
+-- dept 테이블에서 loc 출력, loc이 new york이면 east로 boston이면 east로 else면 etc로 변경한 값을 area로 출력
+select loc, case loc when 'new york' then 'east' when 'boston' then 'east' else 'etc' end as area 
+from dept;
+
+-- emp 테이블에서 ename 이름, sal 급여, comm 커미션, comm이 null이면 0으로 바꿔 sal과 더한 뒤 월지급액 출력
+select ename 이름, sal 급여, comm 커미션, isnull(comm, 0) + sal 월지급액
+from emp;
+
+-- emp 테이블에서 ename, empno, ngr, mgr이 7698이면 null으로 아니면 그대로 nullif이름으로 출력
+select ename, empno, mgr, nullif(mgr, 7698) as nulif
+from emp;
+
+-- player 테이블에서 player_name이 장으로 시작하는 player_name 선수명, position 포지션, back_no 백넘버, height 키 출력
+use K_LEAGUE;
+select player_name as 선수명, position as 포지션, back_no as 백넘버, height as 키
+from player
+where player_name LIKE '장%';
+
+-- player 테이블에서 player_name 가운데 이름이 인인 사람 player_name 선수명, position 포지션, back_no 백넘버, height 키 출력
+select player_name as 선수명, position as 포지션, back_no as 백넘버, height as 키
+from player
+where player_name like '_인_';
+
+-- player 테이블에서 position이 null 인 player_name 선수명, position 포지션, back_no 백넘버, height 키 출력
+select player_name as 선수명, position as 포지션, back_no as 백넘버, height as 키 
+from player
+where position is null;
+
+-- player 테이블에서 team_id가 k02, k07 안에 있고 position이 mf고 height가 170-180 사이인 팀id, 선수명, 포지션, 백넘버, 키 출력
+select team_id, player_name, position, back_no, height
+from player
+where team_id in ('K02', 'K09') and position = 'mf' and height between 170 and 180;
+
+-- player 테이블에서 team_id가 k02이고 position이 mf이 아니고 height가 175-185 아닌 사람의 선수명, 포지션, 백넘버, 키 출력
+select player_name, position, back_no, height
+from player
+where team_id = 'K02' and not position != 'mf' and not height between 175 and 185;
+
+-- Player 테이블에서 nation이 null이 아닌 player_name과 nation 출력
+select player_name, nation
+from player
+where nation is not null;
+
+-- player 테이블에서 전체 행수, 키 건수(height), 최대키, 최소키, 평균키 출력
+select count(*) as 전체행수, count(height) 키건수, max(height) 최대키, min(height), avg(height)
+from player;
+
+-- position이 있는 선수들의 position, height 평균키를 position 별로 출력
+select position, avg(height) 평균키
+from player
+group by POSITION;
+
+-- height의 평균이 180 이상인 선수들의 position, 평균키를 position 별로 출력
+select position, avg(height)
+from player
+group by position
+having avg(height) >= 180
+
+-- team_id가 k02, k09인 선수들의 팀 id, 인원수를 팀별로 분류 (where, having 버전 두개)
+select team_id, count(*) as 인원수
+from player
+where team_id in ('k02', 'k09')
+group by team_id;
+
+select team_id, count(*) as 인원수
+from player
+group by team_id
+having team_id in ('k02', 'k09');
+
+-- team별로 position이 fw이면 1으로 아니면 그대로해서 합한 값fw와 team_id 출력
+select team_id, sum(case position when 'fw' then 1 end) as fw
+from player
+group by team_id;
+
+-- 위에서 position이 null이면 0으로 바꿔서 출력
+select team_id, isnull(sum(case position when 'fw' then 1 end), 0) as fw
+from player
+group by team_id;
+
+-- 위에서 fw, mf, df, gk 포지션 별로 출력
+select team_id, isnull(sum(case position when 'fw' then 1 end), 0) as fw,
+isnull(sum(case position when 'mf' then 1 end), 0) as mf,
+isnull(sum(case position when 'df' then 1 end), 0) as df,
+isnull(sum(case position when 'gk' then 1 end), 0) as gk
+from player
+group by team_id;
+
+-- player테이블에서 back_no가 null이 아닌 player의 선수명, 포지션, 백넘버 출력하는데 백넘버, 포지션, 선수명 으로 내림차순 정렬 (숫자로!)
+SELECT PLAYER_NAME, POSITION, BACK_NO
+FROM PLAYER
+WHERE BACK_NO IS NOT NULL
+ORDER BY 3 DESC, 2, 1;
+
+-- emp 테이블에서 job, 인원수, sal 평균급여 출력하는 데 job 별로 묶고 sal 평균급여가 2500이 초과인 사람 출력. 
+-- Sal 평균 급여 내림차순으로 출력
+USE COMPANY;
+SELECT JOB, COUNT(*) 인원수, AVG(SAL) 평균급여
+FROM EMP
+GROUP BY JOB
+HAVING AVG(SAL) > 2500
+ORDER BY AVG(SAL) DESC;
+
+-- team이랑 player 테이블 team.id 기준으로 inner 조인 후 player_name 선수명, team_name 소속팀명 출력 
+-- (2가지 버전: equi join, ansi/iso sql 표준 조인  inner는 생략 가능)
+USE K_LEAGUE;
+SELECT PLAYER_NAME, TEAM_NAME
+FROM TEAM, PLAYER
+WHERE TEAM.TEAM_ID = PLAYER.TEAM_ID;
+
+SELECT PLAYER_NAME, TEAM_NAME
+FROM TEAM JOIN PLAYER
+ON TEAM.TEAM_ID = PLAYER.TEAM_ID;
+
+-- 위에서 equi join 중 player 를 a로 team을 b로 별칭 주기
+SELECT PLAYER_NAME, TEAM_NAME
+FROM PLAYER A, TEAM B
+WHERE A.TEAM_ID = B.TEAM_ID;
+
+-- player와 team 테이블에서 team_id가 같고 a의 position이 ‘gk’인 데이터를 대상으로 player_name 선수명, back_no 백넘버, region_name 연고지, team_name 팀명 출력
+SELECT PLAYER_NAME, BACK_NO, REGION_NAME
+FROM PLAYER A, TEAM B
+WHERE A.TEAM_ID = B.TEAM_ID AND A.POSITION = 'GK';
+
+-- EMP A 테이블와 SAL_GRADE B 테이블에서 비교연산자로 JOIN
+-- EMP의 ENAME, SAL, SAL_GRADE의 GRADE 급여등급 출력
+-- EMP의 SAL이 B의 LOSAL과 HISAL 사이에 있을 때 급여등급 기준 오름차순 조회
+USE COMPANY;
+SELECT A.ENAME, A.SAL, B.GRADE 급여등급
+FROM EMP A, SAL_GRADE B
+WHERE A.SAL BETWEEN B.LOSAL AND B.HISAL
+ORDER BY 급여등급;
+
+-- 3개 이상 테이블 조인, player a의 team_id과 team b의 team_id가 같고 stadium c와 b의 stadium_id가 같은 데이터 조인
+USE K_LEAGUE;
+SELECT *
+FROM PLAYER A, TEAM B, STADIUM C
+WHERE A.TEAM_ID = B.TEAM_ID AND C.STADIUM_ID = B.STADIUM_ID;
+
+-- emp A의 deptno와 dept B의 deptno를 기준으로 b의 deptno가 30인 데이터 중 a의 empno, a의 ename, b의 deptno, dname 출력
+USE COMPANY;
+SELECT A.DEPTNO, A.ENAME, B.DEPTNO, B.DNAME
+FROM EMP A, DEPT B
+WHERE A.DEPTNO = B.DEPTNO AND B.DEPTNO = 30
+
+SELECT A.DEPTNO, A.ENAME, B.DEPTNO, B.DNAME
+FROM EMP A INNER JOIN DEPT B ON A.DEPTNO = B.DEPTNO 
+WHERE B.DEPTNO = 30
+
+-- emp A의 deptno와 dept B의 deptno를 기준으로 a의 mgr이 7698인 데이터 중 a의 empno, a의 ename, b의 deptno, dname 출력
+SELECT A.EMPNO, A.ENAME, B.DEPTNO, B.DNAME
+FROM EMP A INNER JOIN DEPT B 
+ON A.DEPTNO = B.DEPTNO
+WHERE A.MGR = 7698
+
+-- player a테이블, team B 테이블, stadium C 테이블 조인, a의 position이 ‘gk’일 때, a의 player_name 기준으로 정렬
+USE K_LEAGUE;
+SELECT * FROM PLAYER;
+SELECT * FROM TEAM;
+SELECT * FROM STADIUM;
+
+SELECT *
+FROM PLAYER A JOIN TEAM B
+ON A.TEAM_ID = B.TEAM_ID JOIN STADIUM C ON C.STADIUM_ID = B.STADIUM_ID
+WHERE A.POSITION = 'GK'
+ORDER BY A.PLAYER_NAME;
+
+-- EMP A와 DEPT B 카티션 프로덕트, A의 ENAME 기준으로 정렬. A의 ENAME이랑 B의 DNAME 조회
+USE COMPANY;
+SELECT A.ENAME, B.DNAME
+FROM EMP A CROSS JOIN DEPT B
+ORDER BY A.ENAME;
+
+-- STADIUM A, TEAM B 왼쪽 아우터 조인하는데 A의 STADIUM_NAME, A.STADIUM_ID, A.SEAT_COUNT, A.HOMETEAM_ID, B.TEAM_NAME 조회
+USE K_LEAGUE;
+SELECT A.STADIUM_NAME, A.STADIUM_ID, A.SEAT_COUNT, A.HOMETEAM_ID, B.TEAM_NAME
+FROM STADIUM A LEFT OUTER JOIN TEAM B
+ON A.HOMETEAM_ID = B.TEAM_ID;
+
+SELECT * FROM STADIUM;
+SELECT * FROM TEAM;
+
+-- DEPT로부터 DEPT_TEMP 복사하기
+USE COMPANY;
+SELECT * INTO DEPT_TEMP FROM DEPT;
+
+-- DEPT_TEMP 에서 DNAME이 RESEARCH 인 데이터 CONSULTING으로 바꾸기
+UPDATE DEPT_TEMP
+SET DNAME = 'CONSULTING'
+WHERE DNAME = 'RESEARCH';
+
+-- DEPT_TEMP 에서 DEPTNO에 20 더한 값으로 바꾸기
+UPDATE DEPT_TEMP
+SET DEPTNO += 20;
---
+++ SQL활용_210425.sql
@@ -0,0 +1,151 @@
+﻿-- 7.
+
+-- 서브쿼리
+-- player 테이블에서 정남일 선수의 팀의 player_name 선수명, position 포지션, back_no 백넘버 출력, player_name으로 정렬
+USE K_LEAGUE;
+SELECT PLAYER_NAME, POSITION, BACK_NO
+FROM PLAYER
+WHERE TEAM_ID = (SELECT TEAM_ID FROM PLAYER WHERE PLAYER_NAME = '정남일')
+ORDER BY PLAYER_NAME;
+
+-- player 테이블에서 선수들 평균키 (height이용)보다 큰 키를 가진 선수들의 player_name, position, back_no 출력, player_name으로 정렬
+SELECT PLAYER_NAME, POSITION, BACK_NO
+FROM PLAYER
+WHERE HEIGHT > (SELECT AVG(HEIGHT) FROM PLAYER)
+ORDER BY PLAYER_NAME;
+
+-- team 테이블에서 정현수 선수가 속한 팀의 region_name, team_name, e_team_name 출력
+SELECT REGION_NAME, TEAM_NAME, E_TEAM_NAME
+FROM TEAM
+WHERE TEAM_ID IN (SELECT TEAM_ID FROM PLAYER WHERE PLAYER_NAME = '정현수');
+
+-- player a와 team b 테이블에서 team_id 별로, a의 team_id 팀의 선수들의 평균키보다 a의 height가 작은 데이터 중 
+-- b의 team_name, a의 player_name, a의 position, a의 back_no, a의 height 출력
+SELECT B.TEAM_NAME, A.PLAYER_NAME, A.POSITION, A.BACK_NO, A.HEIGHT
+FROM PLAYER A, TEAM B
+WHERE A.HEIGHT < (SELECT AVG(HEIGHT) FROM PLAYER X WHERE X.TEAM_ID = A.TEAM_ID GROUP BY X.TEAM_ID) 
+AND B.TEAM_ID = A.TEAM_ID
+ORDER BY A.PLAYER_NAME;
+
+-- stadium a 테이블과 schedule x 테이블에서 x와 a의 stadium_id가 같은 값을 만족하고 x의 sche_date가 20120501 과 20120502 사이에 
+-- 있는 데이터의 a의 stadium_id, stadium_name 출력
+SELECT A.STADIUM_ID, A.STADIUM_NAME
+FROM STADIUM A, SCHEDULE X
+WHERE A.STADIUM_ID = X.STADIUM_ID AND X.SCHE_DATE BETWEEN 20120501 AND 20120502;
+
+SELECT A.STADIUM_ID, A.STADIUM_NAME
+FROM STADIUM A
+WHERE EXISTS (SELECT * FROM SCHEDULE X WHERE X.STADIUM_ID = A.STADIUM_ID AND X.SCHE_DATE BETWEEN '20120501' AND '20120502')
+
+-- player a 테이블에서 player_name, height, team_id 팀평균키 출력
+SELECT * FROM PLAYER
+SELECT A.PLAYER_NAME, A.HEIGHT, A.TEAM_ID, (SELECT AVG(HEIGHT) FROM PLAYER X WHERE X.TEAM_ID = A.TEAM_ID) 팀평균키
+FROM PLAYER A;
+
+-- player 테이블에서 position이 mf인 테이블을 a라고 명칭, team을 b라고 별칭 주고 두 table의 team_id가 같을 때 join, 
+-- a의 player_name 기준 정렬
+SELECT *
+FROM (SELECT * FROM PLAYER WHERE POSITION = 'MF') A, team B
+WHERE A.TEAM_ID = B.TEAM_ID
+ORDER BY A.PLAYER_NAME;
+
+-- player a 테이블와 team b 테이블에서 a의 team_id, b의 team_name, a의 평균키 출력하는데 a의 team_id, b의 team_name 별로 그루핑,
+-- 삼성블루윙즈를 team_name으로 가지는 team_id에 속하는 team의 평균키보다 a의 height가 큰 것만 출력
+-- 각 팀의 평균키가 삼성블루윙즈 팀의 평균키보다 작은 팀을 대상으로 A.TEAM_ID, B.TEAM_NAME, 평균키 선택
+ SELECT A.TEAM_ID, B.TEAM_NAME, AVG(A.HEIGHT)
+ FROM PLAYER A, TEAM B
+ GROUP BY A.TEAM_ID, B.TEAM_NAME
+ HAVING AVG(A.HEIGHT) <  (SELECT AVG(HEIGHT) FROM PLAYER X WHERE X.TEAM_ID IN 
+ (SELECT TEAM_ID FROM TEAM WHERE TEAM_NAME = '삼성블루윙즈'));
+
+
+-- player a와 team b 테이블에서 b와 a의 team_id 가 같은 것만 조인한 a의 player_name, position, back_no, b의 team id, 
+-- team_name 출력한 결과를 뷰(v_player_Team)로 생성
+CREATE VIEW V_PLAYER_TEAM AS 
+(SELECT A.PLAYER_NAME, A.POSITION, A.BACK_NO, B.TEAM_ID, B.TEAM_NAME FROM PLAYER A, TEAM B WHERE B.TEAM_ID = A.TEAM_ID);
+
+-- v_player_team에서 position이 ‘gk’, ‘mf’ 인 player_name, position, back_no, team_name 출력결과를 v_player_team_filter 뷰로 정의
+CREATE VIEW V_PLAYER_TEAM_FILTER
+AS (SELECT PLAYER_NAME, POSITION, BACK_NO, TEAM_NAME FROM V_PLAYER_TEAM WHERE POSITION IN ('GK', 'MF'))
+
+-- (합집합) player 테이블에서 team_id가 ‘k07’인 player_name, back_no 결과와 ‘k09’인 결과 출력
+SELECT PLAYER_NAME, BACK_NO
+FROM PLAYER WHERE TEAM_ID = 'K07'
+UNION
+SELECT PLAYER_NAME, BACK_NO
+FROM PLAYER WHERE TEAM_ID = 'K07';
+
+-- 위에서 합집합 명령어 말고 그냥 select만 이용해서 출력해보기
+SELECT DISTINCT PLAYER_NAME, BACK_NO
+FROM PLAYER WHERE TEAM_ID = 'K02' OR TEAM_ID = 'K07';
+
+-- player 테이블에서 team_id가 ‘k02’인 team_id, player_name, position, back_no, height 결과와 position이 ‘gk’인 결과 합치기 
+-- (중복 제거 버전과 중복 제거 x 버전)
+SELECT TEAM_ID, PLAYER_NAME, BACK_NO, HEIGHT FROM PLAYER WHERE TEAM_ID = 'K02'
+UNION
+SELECT TEAM_ID, PLAYER_NAME, BACK_NO, HEIGHT FROM PLAYER WHERE POSITION = 'GK'
+
+SELECT TEAM_ID, PLAYER_NAME, BACK_NO, HEIGHT FROM PLAYER WHERE TEAM_ID = 'K02'
+UNION ALL
+SELECT TEAM_ID, PLAYER_NAME, BACK_NO, HEIGHT FROM PLAYER WHERE POSITION = 'GK'
+
+-- player 테이블에서 position 별로 ‘P’ 구분코드, position, height 평균키 출력한 결과와 team_id 별로 ‘t’ 구분코드
+-- team_id, height 평균키 출력 결과 합친 후 p와 t 구분코드 순서대로 정렬
+SELECT  'P' 구분코드, POSITION, AVG(HEIGHT) FROM PLAYER GROUP BY POSITION
+UNION ALL
+SELECT  'T' 구분코드, TEAM_ID, AVG(HEIGHT) FROM PLAYER GROUP BY TEAM_ID
+ORDER BY 1, 2 DESC;
+
+-- player 테이블에서 team_id가 ‘k02’인 team_id, player_name, position, back_no, height 중에서 position이 mf인 값 빼기
+SELECT TEAM_ID, PLAYER_NAME, POSITION, BACK_NO, HEIGHT FROM PLAYER WHERE TEAM_ID = 'K02'
+EXCEPT SELECT * FROM PLAYER WHERE POSITION = 'MF';
+
+-- Emp a 테이블과 dept b 테이블에서 a의 deptno와 b의 deptno가 같은 값들 b의 dname과 a의 job 별로 출력 
+-- (b.dame, a.job, count(*) emp_cnt, sum(a.sal) sal sum)
+USE COMPANY;
+SELECT B.DNAME, A.JOB, COUNT(*) EMP_CNT, SUM(A.SAL), SUM(A.SAL) SAL_SUM
+FROM EMP A JOIN DEPT B ON A.DEPTNO = B.DEPTNO
+GROUP BY B.DNAME, A.JOB;
+
+-- 위에서 b.name과 a.job 별로 총합 계산 (group function)
+SELECT B.DNAME, A.JOB, COUNT(*) EMP_CNT, SUM(A.SAL), SUM(A.SAL) SAL_SUM
+FROM EMP A JOIN DEPT B ON A.DEPTNO = B.DEPTNO
+GROUP BY ROLLUP (B.DNAME, A.JOB);
+
+
+-- emp 테이블에서 ename, sal 위에서부터 2개 출력하는데 sal 기준 내림차순
+SELECT TOP(2) ENAME, SAL
+FROM EMP
+ORDER BY SAL DESC;
+
+-- 위에서 2등까지 표시하고 공동있으면 그것도 표시
+SELECT TOP(2) WITH TIES ENAME, SAL
+FROM EMP
+ORDER BY SAL DESC;
+
+-- ename, sal 출력하는데 sal, empno로 정렬하고 5개 행 건너뛰고 조회
+SELECT ENAME, SAL
+FROM EMP
+ORDER BY SAL, EMPNO
+OFFSET 5 ROWS
+
+-- emp 테이블에서 ename, sal 조회 sal 내림차순 기준, empno 기준 5개행만 출력
+SELECT ENAME, SAL
+FROM EMP
+ORDER BY SAL DESC,
+EMPNO OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY;
+
+-- 셀프조인하면서 조회, emp 테이블 a, b 조인하는데 jones가 매니저인 직원 조회  empno, ename, mgr 출력
+SELECT A.EMPNO, A.ENAME, A.MGR 
+FROM EMP A, EMP B
+WHERE A.MGR = B.EMPNO AND B.ENAME = 'JONES'
+
+-- emp a, b, c 셀프 조인하는데 매니저가 jones인 애들을 매니저로 두는 직원 출력 empno, ename, mgr
+SELECT C.EMPNO, C.ENAME, C.MGR
+FROM EMP A, EMP B, EMP C
+WHERE A.MGR = B.EMPNO AND B.ENAME = 'JONES' AND C.MGR = A.EMPNO
+
+-- emp a, b, c 셀프 조인 smith의 매니저인	 애들의 매니저 출력
+SELECT C.EMPNO, C.ENAME, C.MGR
+FROM EMP A, EMP B, EMP C
+WHERE A.ENAME = 'SMITH' AND B.EMPNO = A.MGR AND C.EMPNO = B.MGR
